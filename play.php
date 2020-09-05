ed5f7b3b51279496e32ad02c71c34cf9e80fdaf1
zanefinner <zanefinnergm@gmail.com>
2020-09-05 15:31:13 -0400

    plan data handling

--- play.php
+++ play.php
@@ -1,2 +1,15 @@
-<?php
-    echo "play at ".$_GET["world"];
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sandblox - <?= $_GET["world"]?></title>
+    <script src="js/socket_connection.js"></script>
+    <script src="js/app.js"></script>
+    <script> let globalWorld = <?=$_GET["world"]?></script>
+    
+</head>
+<body>
+    
+</body>
+</html>
